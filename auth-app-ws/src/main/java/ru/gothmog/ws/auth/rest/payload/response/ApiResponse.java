1b338697cbb6114cbe6e0ba9e039be1e2d802a2f
dgrushetskiy <dgrushetskiy@gmail.com>
2022-09-11 10:16:33 +0300

    create api response

---
+++ auth-app-ws/src/main/java/ru/gothmog/ws/auth/rest/payload/response/ApiResponse.java
@@ -0,0 +1,13 @@
+package ru.gothmog.ws.auth.rest.payload.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class ApiResponse {
+    private Boolean success;
+    private String message;
+}
